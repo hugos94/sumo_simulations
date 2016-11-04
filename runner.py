61a50f81eeba3257d2013c772fe440948bef8467
Hugo Piauilino <hugos94@gmail.com>
2016-11-04 15:04:56 -0300

    Added traci folder with the implementations for TraCI.

--- runner.py
+++ runner.py
@@ -21,8 +21,8 @@ import random
 try:
     sys.path.append(os.path.join(os.path.dirname(
         __file__), '..', '..', '..', '..', "tools"))  # tutorial in tests
-    sys.path.append(os.path.join(os.environ.get("SUMO_HOME", os.path.join(
-        os.path.dirname(__file__), "..", "..", "..")), "tools"))  # tutorial in docs
+    #sys.path.append(os.path.join(os.environ.get("SUMO_HOME", os.path.join(os.path.dirname(__file__), "..", "..", "..")), "tools"))  # tutorial in docs
+    sys.path.append(os.path.join(os.environ.get("SUMO_HOME", os.path.join("/usr/share/sumo")), "tools")) #Descomentar a linha acima e comentar esta quando utilizar outra maquina
     from sumolib import checkBinary
 except ImportError:
     sys.exit("please declare environment variable 'SUMO_HOME' as the root directory of your sumo installation (it should contain folders 'bin', 'tools' and 'docs')")
